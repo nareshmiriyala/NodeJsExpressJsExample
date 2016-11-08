d1cd7a75b94e5e30d1eb7b4975229134e7b9b5bb
Naresh Miriyala <naresh.miriyala@jemena.com>
2016-11-08 14:24:40 +1100

    working code

--- public/javascripts/controllers/controller.js
+++ public/javascripts/controllers/controller.js
@@ -1,3 +1,12 @@
 todoApp.controller("ToDoCtrl", function ($scope) {
     $scope.todo = model;
+    $scope.incompleteCount = function () {
+        var count = 0;
+        angular.forEach($scope.todo.items, function (item) {
+            if (!item.done) {
+                count++
+            }
+        });
+        return count;
+    }
 });
